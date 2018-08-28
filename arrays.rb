eb19ced3ac6c46f2eff68e2f256bbb6fab09d45a
oephi <r.sickle@protonmail.com>
2018-08-29 09:08:51 +1000

    first commit

--- arrays.rb
+++ arrays.rb
@@ -1,14 +1,47 @@
-# Part I - Print a single item from the following arrays:
+# # Part I - Print a single item from the following arrays:
 
-# Use the following array of Kardashians for exercises 1 - 5
+# # Use the following array of Kardashians for exercises 1 - 5
 kardashians = ['Kim', 'Robert', 'Khloé', 'Rob', 'Kourtney']
 
-# 1) Find two ways to access and print the 'Kim' value.
+# # 1) Find two ways to access and print the 'Kim' value.
+# puts kardashians[0]
+# puts kardashians.first
+# puts "----------------------"
 
-# 2) Find two ways to remove the male names from the Kardashians.
+# # 2) Find two ways to remove the male names from the Kardashians.
+# kardashians.delete_at(1)
+# kardashians.delete_at(2)
+# puts kardashians
+# puts "----------------------"
+# kardashians.delete('Robert')
+# kardashians.delete('Rob')
+# puts kardashians
+# puts "--------------------------"
 
-# 3) Remove one Kardashian, print the names and then add them back to the Array:
+# # 3) Remove one Kardashian, print the names and then add them back to the Array:
+# kim = kardashians.delete('Kim')
+# puts kardashians
+# puts "----------"
+# kardashians.push(kim)
+# puts kardashians
+# puts "-----------------------------"
 
-# 4) Take the last Kardashian and make them the first, print the result.
+# # 4) Take the last Kardashian and make them the first, print the result.
+# kardashians.unshift(kardashians.pop)
 
-# 5) Find a way to count the number of items in the array that begin with the letter 'K'
+# puts kardashians
+
+# # 5) Find a way to count the number of items in the array that begin with the letter 'K'
+def has_k(array)
+    namesWithK = []
+    for i in array
+        if i.start_with? "K"
+            namesWithK.push(i)
+        end
+    end
+    puts namesWithK.length
+end
+
+has_k(kardashians)
+
+
--- loops.rb
+++ loops.rb
@@ -4,6 +4,11 @@ shopping_list = ['cheese', 'toilet paper', 'taco shells', 'sushi rolls', '3 avoc
 # 1) Use .each to loop through each item in the shopping_list and capitalise
 #    all words in each item. Hint: work smarter not harder - check Stack Overflow
 
+# shopping_list.each do |i|
+#     puts i.split.map(&:capitalize).join(' ')
+# end
+
+
 # 2) Use while to loop through the shopping list and divide the shopping list
 #    into multiple arrays according to item type. Print all of the arrays with a divider
 #    between each and a total item count for each item.
@@ -14,12 +19,85 @@ dairy = []
 meals = []
 beverages = []
 
+i = 0
+count = 0
+while i < shopping_list.length     
+    if shopping_list[i] == 'cheese'
+        dairy.push('cheese')
+    elsif shopping_list[i]  == 'toilet paper' 
+        bathroom_items.push('toilet paper')
+    elsif shopping_list[i] == 'soap'
+        bathroom_items.push('soap')
+    elsif shopping_list[i] == 'toothpaste'
+        bathroom_items.push('toothpaste')
+    elsif shopping_list[i] == '3 avocados'
+        fruit.push('3 avocados')
+    elsif shopping_list[i] == '2 pineapples'
+        fruit.push('2 pineapples')
+    elsif shopping_list[i] == 'taco shells'
+        meals.push('taco shells')
+    elsif shopping_list[i] == 'sushi rolls'
+        meals.push('sushi rolls')
+    elsif shopping_list[i] == 'sparkling mineral water'
+        beverages.push('sparkling mineral water')
+    end
+    i+=1
+end
+
+puts dairy
+puts "Item count for dairy is #{dairy.count} item"
+puts "----------------"
+puts bathroom_items
+puts "Item count for bathroom_items is #{bathroom_items.count} items"
+puts "----------------"
+puts fruit
+puts "Item count for fruit is #{fruit.count} items"
+puts "----------------"
+puts meals
+puts "Item count for meals is #{meals.count} items"
+puts "----------------"
+puts beverages
+puts "Item count for beverages is #{beverages.count} item"
+
 # 3) Create an array of at least 20 integers.
 #       Use a for loop to:
 #         - find the average of all the integers
 #         - find the sum of all the integers
 #         - create an array of the same values as floats
 #         - create an array of values that are multiplied by a randomly generated number
+
+# integers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# sum = 0
+# for i in integers
+#     sum += i
+# end
+
+# floats = []
+# for i in integers
+#     floats.push(i.to_f)
+# end
+
+# random = []
+# for i in integers
+#     random.push(i * rand(1000))
+# end
+
+# puts "create an array of values that are multiplied by a randomly generated number"
+# puts random
+# puts ""
+
+# puts "create an array of the same values as floats"
+# puts floats
+# puts ""
+
+# puts "find the average of all the integers"
+# puts sum / 20
+# puts ""
+
+# puts "find the sum of all the integers"
+# puts sum
+
 
 # EXTENSION TASK
 # Use a for loop to go through each item in the shopping_list, remove any number in the
